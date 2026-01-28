c3e745028aba79c23cf0ef539db382645660233c
Sofia Ortega <s.ortega@tamu.edu>
2026-01-27 21:58:32 -0600

    Replacing words

--- src/content.ts
+++ src/content.ts
@@ -6,7 +6,17 @@ const SKIP_TAGS = new Set([
   "IFRAME",
 ]);
 
-function walkAndReplace(node: Node, translations: Record<string, string>) {
+const CONTENT_TRANSLATION_KEY = "translations";
+
+interface ITranslationStorage {
+  replace: string;
+  disable: boolean;
+}
+
+function walkAndReplace(
+  node: Node,
+  translations: Record<string, ITranslationStorage>,
+) {
   // Only process text nodes
   if (
     node.nodeType === Node.TEXT_NODE &&
@@ -17,17 +27,19 @@ function walkAndReplace(node: Node, translations: Record<string, string>) {
   if (node.nodeType === Node.TEXT_NODE) {
     let text = node.textContent ?? "";
 
-    for (const [word, replacement] of Object.entries(translations)) {
+    for (const [word, data] of Object.entries(translations)) {
+      if (data.disable) continue;
+
       const wordRegex = new RegExp(`\\b${word}\\b`, "gi"); // 'g' + 'i' for global + case-insensitive
 
       text = text.replace(wordRegex, (match) => {
         const isCapitalized = match[0] === match[0].toUpperCase();
-        const replaced = isCapitalized
-          ? replacement[0].toUpperCase() + replacement.slice(1)
-          : replacement;
+        const replacement = isCapitalized
+          ? data.replace[0].toUpperCase() + data.replace.slice(1)
+          : data.replace;
 
-        console.log(`Replacing "${match}" with "${replaced}"`);
-        return replaced;
+        console.log(`Replacing "${match}" with "${replacement}"`);
+        return replacement;
       });
     }
 
@@ -42,18 +54,15 @@ function walkAndReplace(node: Node, translations: Record<string, string>) {
   }
 }
 
-// Start from the <body> of the document
-const translations: Record<string, string> = {
-  chrome: "mwhaahah",
-  scripts: "swoosh",
-  inject: "hiyaaaa",
+const loadTranslationsLocalStorage = () => {
+  chrome.storage.local.get(CONTENT_TRANSLATION_KEY, (result) => {
+    const translations: Record<string, ITranslationStorage> =
+      result[CONTENT_TRANSLATION_KEY] || {};
+
+    if (document.body) {
+      walkAndReplace(document.body, translations);
+    }
+  });
 };
 
-if (document.body) {
-  console.log("translations:", translations);
-  chrome.storage.local.get("userInputs", function (result: StorageData) {
-    console.log(result);
-  });
-
-  walkAndReplace(document.body, translations);
-}
+loadTranslationsLocalStorage();
